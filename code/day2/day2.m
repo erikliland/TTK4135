fb55208d4573bfe1e42976a727ff64f4b37d38a4
lightbits <simenh.jobb@gmail.com>
2015-02-26 16:06:11 +0100

    Slightly more work on day2.
    code/day2/day2.m:
    	Tried to compute the matrices necessary for the QP. Failed.
    help/genmatrix/*
    	Some useful MATLAB scripts that we are allowed to use without
    	documentation in the report.

--- code/day2/day2.m
+++ code/day2/day2.m
@@ -2,6 +2,7 @@ run('../init.m');
 
 h = 0.25; % timestep
 N = 100;  % number of samples
+q = 0.1;
 
 % Continuous time matrices
 Ac = [0 1 0 0 ; 0 0 -K_2 0 ; 0 0 0 1 ; 0 0 -K_1*K_pp -K_1*K_pd];
@@ -10,17 +11,38 @@ Bc = [0 ; 0 ; 0 ; K_1*K_pp];
 % Discrete time matrices (forward Euler difference approx)
 A = eye(4) + h * Ac;
 B = h * Bc;
+
 %Upper and lower bounds. First row is states, second row is inputs
-% ub = [Inf(1, N)';30*pi/180 * ones(1,N)'];
-% lb = [-Inf(1,N)';-30*pi/180 * ones(1,N)'];
-%ub(2,end)=0; %Last output should be zero
-%lb(2,end)=0; %Last output should be zero
 
-x0 = [pi 0 0 0];
-% H = kron(eye(N),2*diag([1 0 0 0]));
-q = 0.1;
+ubx = zeros(1, 4 * N);
+ubu = zeros(1, N);
+lbx = zeros(1, 4 * N);
+lbu = zeros(1, N);
 
-%[x, fval, exitflag, output, lambda] = .../../help/quadprog(H,[],[],[],Aeq,Beq,lb,ub,x0);
+j = 1;
+for i=1:N
+    r = [Inf(1, 1) Inf(1, 1) 30*pi/180 Inf(1, 1)];
+    ubx(j:j+3) = r;
+    lbx(j:j+3) = -r;
+    j = j + 4;
+end
+
+ubu = ones(1, N) * 30 * pi / 180;
+lbu = -ones(1, N) * 30 * pi / 180;
+
+ub = [ubx ubu];
+lb = [lbx lbu];
+
+Aeq = gena2(A, B, 100, 4, 1);
+Beq = zeros(4 * N, 1);
+
+Q1 = [1 0 0 0 ; 0 0 0 0 ; 0 0 0 0 ; 0 0 0 0];
+P1 = [q];
+H = genq2(Q1, P1, N, N, 1);
+
+f = zeros(1, 5 * N);
+x0 = [pi ; 0 ; 0 ; 0];
+[X, fval, exitflag, output, lambda] = quadprog(H, f, [], [], Aeq, Beq, lb, ub, x0);
 
 t = 0:h:N*h-h;
 u = zeros(1, N);
