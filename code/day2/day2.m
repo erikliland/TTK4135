efe8dbf86a04dab602a1b196b7e74f150c1592d3
erikliland <erik.liland@gmail.com>
2015-03-01 17:11:58 +0100

    Fixed bug at upper and lower bound (travel) at last timestep. Now the simulated travel ends at the desired target at the end of the horizon.

--- code/day2/day2.m
+++ code/day2/day2.m
@@ -40,6 +40,8 @@ H = genq2(Q, R, N, N, nu);
 
 % Solve QP
 [lb, ub] = genbegr2(N, N, x_min, x_max, u_min, u_max);
+lb(nx*(N-1)+1) = 0;
+ub(nx*(N-1)+1) = 0;
 f = zeros(1, n);
 z = quadprog(H, f, [], [], Aeq, Beq, lb, ub);
 u = z(N*nx+1:n);
@@ -47,10 +49,11 @@ u = z(N*nx+1:n);
 % Plot simulated system
 sim_travel = z(1:4:N*nx);
 sim_pitch  = z(3:4:N*nx);
-hold all;
 time = (0:N-1)*h;
-plot(time, (180/pi)*sim_travel, 'LineWidth', 2);
-plot(time, (180/pi)*sim_pitch, 'LineWidth', 2);
+hold off;
+plot(time, (180/pi)*sim_travel,'b', 'LineWidth', 2);
+hold on;
+plot(time, (180/pi)*sim_pitch,'r', 'LineWidth', 2);
 legend('Travel', 'Pitch');
 axis square;
 box  on;
