688866dcc97494307c22f9ec6397816a98d55e20
erikliland <erik.liland@gmail.com>
2015-03-03 10:01:50 +0100

    Dag 2 fungererer, men responsen er langt fra ønsket

--- code/day2/day2.m
+++ code/day2/day2.m
@@ -8,6 +8,7 @@ h  = 0.25;        % Discretization timestep
 N  = 100;         % Length of horizon
 nx = 4;           % Number of states of system
 nu = 1;           % Number of inputs of system
+n_offset = 5/h;   % Deadzone at start and end (timesteps)
 
 % System state and input bounds
 pitch_lim = 30*pi/180;
@@ -21,12 +22,18 @@ u_min = -pitch_lim;
 n  = N * (nx + nu); % Length of manipulation variable vector
 
 % Continuous-time system matrices
-Ac = [0 1 0 0 ; 0 0 -K_2 0 ; 0 0 0 1 ; 0 0 -K_1*K_pp -K_1*K_pd];
+Ac = [0 1     0         0 ;
+      0 0   -K_2        0 ;
+      0 0     0         1 ;
+      0 0 -K_1*K_pp -K_1*K_pd];
 Bc = [0 ; 0 ; 0 ; K_1*K_pp];
 
 % Discrete-time system matrices
-A = eye(4) + h * Ac;
-B = h * Bc;
+% A = eye(4) + h * Ac;
+% B = h * Bc;
+M = expm(h*[Ac Bc ; 0 0 0 0 0]);
+A = M(1:4, 1:4);
+B = M(1:4, 5);
 
 % Generate equality constraints matrix
 Aeq = gena2(A, B, N, nx, nu);
@@ -47,26 +54,44 @@ z = quadprog(H, f, [], [], Aeq, Beq, lb, ub);
 u = z(N*nx+1:n);
 
 % Plot simulated system
+figure(1); clf(1);
 sim_travel = z(1:4:N*nx);
 sim_pitch  = z(3:4:N*nx);
-time = (0:N-1)*h;
-hold off;
-plot(time, (180/pi)*sim_travel,'b', 'LineWidth', 2);
-hold on;
-plot(time, (180/pi)*sim_pitch,'r', 'LineWidth', 2);
+time = (0:N-1+2*n_offset)*h;
+hold all;
+plot(time, (180/pi)*[ones(n_offset,1)*pi ; sim_travel ; zeros(n_offset,1)], 'LineWidth', 2);
+plot(time, (180/pi)*[zeros(n_offset,1) ; sim_pitch ; zeros(n_offset,1)], 'LineWidth', 2);
+hx = graph2d.constantline(5,'LineStyle', '--','Color','black', 'LineWidth',2);
+changedependvar(hx,'x');
 legend('Travel', 'Pitch');
 axis square;
 box  on;
-grid on;
-xlim([0 N*h]);
-xlabel('time (seconds)');
-ylabel('angle (degrees)');
+xlim([0 (N+2*n_offset)*h]);
+xlabel('Time [s]');
+ylabel('Agle [deg]');
 title(sprintf('Simulation of system over %d-length horizon', N));
 
-% t = 0:h:N*h-h;
-% u = zeros(1, N);
-% u(51:N) = ones(1, 50) * 0.5;
-% 
-% pitch_input = zeros(N, 2);
-% pitch_input(:, 1) = t;
-% pitch_input(:, 2) = u;
+
+t = (0:N+2*n_offset-1) * h;
+
+pitch_input = zeros(N+2*n_offset, 2);
+pitch_input(:, 1) = t;
+pitch_input(n_offset+1:N+n_offset, 2) = u;
+
+elev_input = zeros(N+2*n_offset,2);
+elev_input(:,1)=t;
+elev_input(end,2) = -5*(pi/180);
+
+%% Plot results
+load ('measurements.mat');
+t = measurements(1,:);
+travel = (180/pi)*measurements(2,:);
+pitch = (180/pi)*measurements(4,:);
+plot(t,travel, 'LineWidth', 2,'LineStyle','--');
+plot(t, pitch, 'LineWidth', 2, 'LineStyle', '--');
+legend('Sim Travel', 'Sim Pitch','Real Travel', 'Real Pitch');
+xlabel('Time [s]');
+ylabel('Agle [deg]');
+title('Simulated optimal trajectory without feedback');
+axis square;
+box  on;
