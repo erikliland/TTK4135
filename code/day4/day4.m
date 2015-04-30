886deb3c421d7b5137de4c2c178954e602c18ebd
erikliland <erik.liland@gmail.com>
2015-04-30 13:40:52 +0200

    Cramped in matlab code on max 2 pages

--- code/day4/day4.m
+++ code/day4/day4.m
@@ -1,5 +1,3 @@
-run('../init.m');
-
 % State vector
 % x1: travel
 % x2: travel rate
@@ -11,9 +9,8 @@ run('../init.m');
 % Input vector
 % u1: pitch setpoint
 % u2: elevation setpoint
-
+run('../init.m');
 global N alpha beta travel_t nx
-
 % Adjustable parameters
 x0 = [pi 0 0 0 0 0]';       % Initial state
 h  = 0.25;                  % Discretization timestep
@@ -25,12 +22,10 @@ nu = 2;                     % Number of inputs of system
 nz = N*(nx+nu);             % Size of z
 Q  = diag([1 0 0 0 0 0]); % State penalty weights
 R  = diag([1 1]);           % Input penalty weight
-
 % Non-linear elevation constraint params
 alpha    = 0.2;
 beta     = 20;
 travel_t = 2*pi/3;
-
 % System state and input bounds
 pitch_lim = 30*pi/180;
 pitch_rate_lim = 30*pi/180;
@@ -38,7 +33,6 @@ x_max = [+inf +inf +pitch_lim +pitch_rate_lim +inf +inf]';
 x_min = [-inf -inf -pitch_lim -pitch_rate_lim -inf -inf]';
 u_max = [+pitch_lim +inf]';
 u_min = [-pitch_lim -inf]';
-
 % Continuous-time system matrices
 Ac = [0 1     0         0      0         0     ;
       0 0   -K_2        0      0         0     ;
@@ -48,11 +42,9 @@ Ac = [0 1     0         0      0         0     ;
       0 0     0         0  -K_3*K_ep -K_3*K_ed];
   
 Bc = [0 0 ; 0 0 ; 0 0 ; K_1*K_pp 0 ; 0 0 ; 0 K_3*K_ep];
-
 % Discrete-time system matrices
 A = eye(6) + h * Ac;
 B = h * Bc;
-
 % Solve QP
 Aeq            = gena2(A, B, N, nx, nu);
 Beq            = zeros(N*nx, 1);
@@ -61,22 +53,17 @@ H              = genq2(Q, R, N, N);
 [lb, ub]       = genbegr2(N, N, x_min, x_max, u_min, u_max);
 lb(nx*(N-1)+1) = 0;
 ub(nx*(N-1)+1) = 0;
-
 f = @(z) z' * H * z;
 final_e = alpha*exp(-beta*(pi-travel_t)^2);
 final_x = [0 0 0 0 final_e+0.1 0]';
 final_u = [0 final_e+0.1]';
-z0 = [x0 ; 
-      repmat(final_x, N-1, 1);
-      repmat(final_u, N, 1)];
-  options = optimset('Algorithm','sqp');
+z0 = [x0;repmat(final_x, N-1, 1);repmat(final_u, N, 1)];
+options = optimset('Algorithm','sqp');
 z = fmincon(f, z0, [], [], Aeq, Beq, lb, ub, @confun,options);
-
 % LQR
 Q_LQR = diag([20 1 1 1 30 10]);
 R_LQR = diag([1 1]);
 [K, S, E] = dlqr(A,B,Q_LQR,R_LQR);
-
 % Create Simulink inputs
 t = (0:N+2*n_offset-1) * h;
 u = [z(N*nx+1:nu:nz) z(N*nx+2:nu:nz)];
@@ -98,9 +85,7 @@ x_star(n_offset+1:N+n_offset, 7) = z(6:nx:N*nx);
 %% Plot simulated trajectory and input
 fig = figure(1); clf(1);
 set(gca,'FontSize',11)
-
-subplot(2,1,1);
-hold all;
+subplot(2,1,1); hold all;
 sim_travel = z(1:nx:N*nx);
 sim_elev   = z(5:nx:N*nx);
 pitch_ref  = z(N*nx+1:nu:N*(nx+nu));
@@ -110,9 +95,7 @@ plot(t, u_star(:,2)*180/pi,  'LineWidth', 2);
 legend('x_t^*','u_p^*');
 xlabel('Time [s]');
 ylabel('Angle [deg]');
-
-subplot(2,1,2);
-hold all;
+subplot(2,1,2); hold all;
 c = alpha*exp(-beta*(sim_travel-travel_t).^2);
 plot(t, x_star(:,6)*180/pi,   'LineWidth', 2);
 plot(t, u_star(:,3)*180/pi,   'LineWidth', 2);
@@ -120,10 +103,8 @@ plot(t, [zeros(n_offset,1); c*180/pi; zeros(n_offset,1)], '--', 'LineWidth', 2);
 legend('x_e^*', 'u_e^*', 'Constraint');
 xlabel('Time [s]');
 ylabel('Angle [deg]');
-
 % Plot results
-%load ('measurements.mat');
-%load ('inputs.mat');
+%load ('measurements.mat'); load ('inputs.mat');
 %save (sprintf('../../measurements/day4/measurements_q_%d_%d_%d_%d_%d_%d.mat', Q_LQR(1,1), Q_LQR(2,2), Q_LQR(3,3), Q_LQR(4,4), Q_LQR(5,5), Q_LQR(6,6)),'measurements');
 %save (sprintf('../../measurements/day4/inputs_q_%d_%d_%d_%d_%d_%d.mat', Q_LQR(1,1), Q_LQR(2,2), Q_LQR(3,3), Q_LQR(4,4), Q_LQR(5,5), Q_LQR(6,6)),'inputs');
 load(sprintf('../../measurements/day4/measurements_q_%d_%d_%d_%d_%d_%d.mat', Q_LQR(1,1), Q_LQR(2,2), Q_LQR(3,3), Q_LQR(4,4), Q_LQR(5,5), Q_LQR(6,6)));
@@ -133,30 +114,19 @@ travel = (180/pi)*measurements(2,:);
 elevation = (180/pi)*measurements(6,:);
 pitchInput = (180/pi)*inputs(2,:);
 elevationInput = (180/pi)*inputs(3,:);
-
-subplot(2,1,1);
-hold all;
+subplot(2,1,1); hold all;
 plot(t_real,travel, 'LineWidth', 2,'LineStyle','--');
 plot(t_real, pitchInput, 'LineWidth', 2, 'LineStyle', '--');
 legend('x_t^*', 'u_p^*','x_t','u_p');
 title('Simulated optimal trajectory with feedback','FontSize',14,'FontWeight','normal');
 line([offsetTime offsetTime],get(gca,'YLim'),'Color','Black','LineWidth',1);
 line([offsetTime+N*h offsetTime+N*h],get(gca,'YLim'),'Color','Black','LineWidth',1);
-xlim([4 (N+2*n_offset)*h]);
-ylim([-50 210]);
-
-subplot(2,1,2);
-hold all;
+xlim([4 (N+2*n_offset)*h]); ylim([-50 210]);
+subplot(2,1,2); hold all;
 plot(t_real,elevation, 'LineWidth', 2,'LineStyle','--');
 legend('x_e^*','u_e^*','Constraint','x_e');
 line([offsetTime offsetTime],get(gca,'YLim'),'Color','Black','LineWidth',1);
 line([offsetTime+N*h offsetTime+N*h],get(gca,'YLim'),'Color','Black','LineWidth',1);
-xlim([4 (N+2*n_offset)*h]);
-ylim([-5 14]);
-
+xlim([4 (N+2*n_offset)*h]); ylim([-5 14]);
 % Set the dimensions of the figure
-set(fig, 'units','centimeters');
-pos = get(gcf, 'position');
-set(gcf, 'position', [pos(1), pos(2), 15, 12]);
-
-%saveas(fig, sprintf('../../figures/day4_cl/plot_q_%d_%d_%d_%d_%d_%d', Q_LQR(1,1), Q_LQR(2,2), Q_LQR(3,3), Q_LQR(4,4), Q_LQR(5,5), Q_LQR(6,6)),'epsc');
+set(fig, 'units','centimeters'); pos = get(gcf, 'position'); set(gcf, 'position', [pos(1), pos(2), 15, 12]);
--- code/day4_openloop/day4_openloop.m
+++ code/day4_openloop/day4_openloop.m
@@ -1,5 +1,3 @@
-run('../init.m');
-
 % State vector
 % x1: travel
 % x2: travel rate
@@ -11,9 +9,8 @@ run('../init.m');
 % Input vector
 % u1: pitch setpoint
 % u2: elevation setpoint
-
+run('../init.m');
 global N alpha beta travel_t nx
-
 % Adjustable parameters
 x0 = [pi 0 0 0 0 0]';       % Initial state
 h  = 0.25;                  % Discretization timestep
@@ -25,12 +22,10 @@ nu = 2;                     % Number of inputs of system
 nz = N*(nx+nu);             % Size of z
 Q  = diag([1 0 0.1 0 0 0]); % State penalty weights
 R  = diag([1 1]);           % Input penalty weight
-
 % Non-linear elevation constraint params
 alpha    = 0.2;
 beta     = 20;
 travel_t = 2*pi/3;
-
 % System state and input bounds
 pitch_lim = 30*pi/180;
 pitch_rate_lim = 30*pi/180;
@@ -38,7 +33,6 @@ x_max = [+inf +inf +pitch_lim +pitch_rate_lim +inf +inf]';
 x_min = [-inf -inf -pitch_lim -pitch_rate_lim -inf -inf]';
 u_max = [+pitch_lim +inf]';
 u_min = [-pitch_lim -inf]';
-
 % Continuous-time system matrices
 Ac = [0 1     0         0      0         0     ;
       0 0   -K_2        0      0         0     ;
@@ -48,11 +42,9 @@ Ac = [0 1     0         0      0         0     ;
       0 0     0         0  -K_3*K_ep -K_3*K_ed];
   
 Bc = [0 0 ; 0 0 ; 0 0 ; K_1*K_pp 0 ; 0 0 ; 0 K_3*K_ep];
-
 % Discrete-time system matrices
 A = eye(6) + h * Ac;
 B = h * Bc;
-
 % Solve QP
 Aeq            = gena2(A, B, N, nx, nu);
 Beq            = zeros(N*nx, 1);
@@ -61,17 +53,13 @@ H              = genq2(Q, R, N, N);
 [lb, ub]       = genbegr2(N, N, x_min, x_max, u_min, u_max);
 lb(nx*(N-1)+1) = 0;
 ub(nx*(N-1)+1) = 0;
-
 f = @(z) z' * H * z;
 final_e = alpha*exp(-beta*(pi-travel_t)^2);
 final_x = [0 0 0 0 final_e+0.1 0]';
 final_u = [0 final_e+0.1]';
-z0 = [x0 ; 
-      repmat(final_x, N-1, 1);
-      repmat(final_u, N, 1)];
+z0 = [x0;repmat(final_x, N-1, 1);repmat(final_u, N, 1)];
 options = optimset('Algorithm','sqp');
 z = fmincon(f, z0, [], [], Aeq, Beq, lb, ub, @confun,options);
-
 % Create Simulink inputs
 t = (0:N+2*n_offset-1) * h;
 u = [z(N*nx+1:nu:nz) z(N*nx+2:nu:nz)];
@@ -123,7 +111,6 @@ t_real = measurements(1,:);
 travel = (180/pi)*measurements(2,:);
 pitch = (180/pi)*measurements(4,:);
 elevation = (180/pi)*measurements(6,:);
-
 subplot(2,1,1);
 hold all;
 plot(t_real,travel, 'LineWidth', 2,'LineStyle','--');
@@ -132,22 +119,15 @@ legend('Opt travel traj', 'Opt pitch ref','Real Travel', 'Real Pitch');
 title('Simulated optimal trajectory without feedback','FontSize',14,'FontWeight','normal');
 line([offsetTime offsetTime],get(gca,'YLim'),'Color','Black','LineWidth',1);
 line([offsetTime+N*h offsetTime+N*h],get(gca,'YLim'),'Color','Black','LineWidth',1);
-xlim([4 (N+2*n_offset)*h]);
-ylim([-140 205]);
-
+xlim([4 (N+2*n_offset)*h]); ylim([-140 205]);
 subplot(2,1,2);
 hold all;
 plot(t_real,elevation, 'LineWidth', 2,'LineStyle','--');
 legend('Opt elev traj', 'Opt elev ref', 'Constraint','Real elevation');
 line([offsetTime offsetTime],get(gca,'YLim'),'Color','Black','LineWidth',1);
 line([offsetTime+N*h offsetTime+N*h],get(gca,'YLim'),'Color','Black','LineWidth',1);
-xlim([4 (N+2*n_offset)*h]);
-ylim([-5 14]);
-
+xlim([4 (N+2*n_offset)*h]); ylim([-5 14]);
 % Set the dimensions of the figure
 set(fig, 'units','centimeters');
 pos = get(gcf, 'position');
 set(gcf, 'position', [pos(1), pos(2), 15, 12]);
-
-%saveas(h,'../../figures/day4_ol/plot','epsc');
-
